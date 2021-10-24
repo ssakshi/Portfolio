e36f50681b092e6cecde246e265ce9996f5704f5
ssakshi <ssakshi@vmware.com>
2021-10-24 16:27:28 -0400

    Update

---
+++ build/precache-manifest.a8954732140331b056a05d0580e1c39c.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94c0980e20731ba7d3e4186c2dbbe8b6",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "e7e4dac8e1095d4ef000",
+    "url": "/portfolio/static/css/3.25727338.chunk.css"
+  },
+  {
+    "revision": "a36842b09633a9bfe246",
+    "url": "/portfolio/static/css/4.a8329f31.chunk.css"
+  },
+  {
+    "revision": "9417cde60aa09d154075",
+    "url": "/portfolio/static/css/main.60ac603a.chunk.css"
+  },
+  {
+    "revision": "cc1c547766be292f9c85",
+    "url": "/portfolio/static/js/2.495966d3.chunk.js"
+  },
+  {
+    "revision": "b9941b26ce7f87a25d1a3bc0d5ae97bc",
+    "url": "/portfolio/static/js/2.495966d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7e4dac8e1095d4ef000",
+    "url": "/portfolio/static/js/3.de0e9b02.chunk.js"
+  },
+  {
+    "revision": "a36842b09633a9bfe246",
+    "url": "/portfolio/static/js/4.fa39c058.chunk.js"
+  },
+  {
+    "revision": "9417cde60aa09d154075",
+    "url": "/portfolio/static/js/main.80f86e4a.chunk.js"
+  },
+  {
+    "revision": "ed65086eeb657719e798",
+    "url": "/portfolio/static/js/runtime-main.0b03efdf.js"
+  },
+  {
+    "revision": "a3752c4bf72688b3db38d0911cf75f61",
+    "url": "/portfolio/static/media/Agustina.a3752c4b.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/portfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "c59720d63dc3595a12e1c7e641a1bfa5",
+    "url": "/portfolio/static/media/airbnbLogo.c59720d6.png"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/portfolio/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "33bc5ceb985ef36d1d551441620bdc23",
+    "url": "/portfolio/static/media/dataservices.33bc5ceb.PNG"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/portfolio/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "64fadea92880efb6806989f05601fd57",
+    "url": "/portfolio/static/media/earltestframework.64fadea9.PNG"
+  },
+  {
+    "revision": "d66bd29bca5afa5aae0711194ca207af",
+    "url": "/portfolio/static/media/engineeringmodernization.d66bd29b.PNG"
+  },
+  {
+    "revision": "f35c3f3478dacbb388c5e41c8e67749a",
+    "url": "/portfolio/static/media/git.f35c3f34.PNG"
+  },
+  {
+    "revision": "48a1a45dbf8bb5e6b73161e77a9fceb1",
+    "url": "/portfolio/static/media/juitlogo.48a1a45d.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/portfolio/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "41b7afe8205ba92b5bbbcbf644af2500",
+    "url": "/portfolio/static/media/microservicechasis.41b7afe8.PNG"
+  },
+  {
+    "revision": "feda39da812f6e6a779ab47c90df8a4c",
+    "url": "/portfolio/static/media/qtweet.feda39da.PNG"
+  },
+  {
+    "revision": "a4254a74d3f36223ea3440954daedae2",
+    "url": "/portfolio/static/media/rmslogo.a4254a74.png"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/portfolio/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/portfolio/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "4f34458c99241eecc3f9731ad8200f1d",
+    "url": "/portfolio/static/media/uflogo.4f34458c.png"
+  }
+]);
